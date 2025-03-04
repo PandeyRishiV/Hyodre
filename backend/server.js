b14b81795e1ba2d60abfa46d2ade42a2b200bb54
Rishi Pandey <pandeyrishiv@gmail.com>
2025-03-04 10:24:08 +0000

    feat(app): update server.js with mongoose and enable cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const mongoClient = require('mongodb').MongoClient;
+
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+app.use(cors()); // TODO Configure cors
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
